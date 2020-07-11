1492366e2f2649204d90450d4a97a070ae75c654
Leonardo Setti <leonardosetti@protonmail.com>
2020-07-11 16:02:45 -0500

    lst cmmt from dc-user

--- startTest.py
+++ startTest.py
@@ -1,6 +1,15 @@
+import platform
+import os
 import this
+
+print("First steps in Python code.")
+print(os.name)
+print(platform.architecture())
+print(platform.system())
+print(platform.release())
 
 
 myWords = 'ALL FINE!'
 print(myWords)
 print(len(myWords))
+print("End of program.")
