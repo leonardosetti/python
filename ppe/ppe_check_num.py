b34876a9ff1de357738323fe62f2971300e89106
Leonardo Setti <leonardosetti@protonmail.com>
2020-08-05 05:51:13 -0500

    sync names
    (cherry picked from commit c9a709bd2b805ede517b5b5b3107e7c9e69fb45a)

--- ppe/ppe_check_num.py
+++ ppe/ppe_check_num.py
@@ -13,7 +13,7 @@ inp_end_i_num = eval(input(f"Informe o último número da série: \n"))
 srl_num = range(inp_start_i_num, inp_end_i_num + 1)
 
 if inp_start_i_num % 2 != 0:
-    print(f"O número inicial da série é ímpar: {inp_start_i_num}")
+    print(f"O número inicial da série é ímpar: {inp_start_i_num}");
 else:
     print(f"O número inicial da série é par: {inp_start_i_num}")
 
